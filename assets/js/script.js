67405426b02385c909e24104bcb405fd2f18f8e7
Stella Zenon <stellazenon.gmail.com>
2022-05-05 10:42:32 +0200

    js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,28 @@
-let bold = document.getElementById('bold');
-let color = document.getElementById('color');
-let wigth = document.getElementById('wigth');
-let text = document.getElementById('text');
+let btnAdd = document.getElementById('btn+');
+let btnSub = document.getElementById('btn-');
+let btnModulo = document.getElementById('btn%');
+let btnX = document.getElementById('btnx');
+let btn1 = document.getElementById('btn1');
+let btn2 = document.getElementById('btn2');
+let btn3 = document.getElementById('btn3');
+let btn4 = document.getElementById('btn4');
+let btn5 = document.getElementById('btn5');
+let btn6 = document.getElementById('btn6');
+let btn7 = document.getElementById('btn7');
+let btn8 = document.getElementById('btn8');
+let btn9 = document.getElementById('btn9');
+let btnCE = document.getElementById('btnCE');
+let btnEq = document.getElementById('btn=');
+let calcul = document.getElementById('calcul');
+let val1 = document.getElementById('result1');
+let val2 = document.getElementById('result2');
+let val3 = document.getElementById('result3');
 
-bold.addEventListener('click', function() {
-    // console.log('ok');
-    text.classList.toggle('fw-bold');
-});
-color.addEventListener('click', function() {
-    text.classList.toggle('text-danger');
-});
-wigth.addEventListener('click', function() {
-    text.classList.toggle('fs-2');
-});
+function getNumber(val) {
+    calcul.innerHTML += val
+}
+function getAdd() {
+    // calcul.innerHTML +=
+}
 
+// btn1.addEventListener('click', getNumber(val1))
