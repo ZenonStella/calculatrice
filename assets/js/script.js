180c347e3a2a2e698d079794e45f3bb5d394d3aa
Stella Zenon <stellazenon.gmail.com>
2022-05-05 11:14:29 +0200

    operators

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,27 @@
-let btnAdd = document.getElementById('btn+');
-let btnSub = document.getElementById('btn-');
-let btnModulo = document.getElementById('btn%');
-let btnX = document.getElementById('btnx');
-let btn1 = document.getElementById('btn1');
-let btn2 = document.getElementById('btn2');
-let btn3 = document.getElementById('btn3');
-let btn4 = document.getElementById('btn4');
-let btn5 = document.getElementById('btn5');
-let btn6 = document.getElementById('btn6');
-let btn7 = document.getElementById('btn7');
-let btn8 = document.getElementById('btn8');
-let btn9 = document.getElementById('btn9');
-let btnCE = document.getElementById('btnCE');
-let btnEq = document.getElementById('btn=');
-let calcul = document.getElementById('calcul');
-let val1 = document.getElementById('result1');
-let val2 = document.getElementById('result2');
+// let btnAdd = document.getElementById('btn+');
+// let btnSub = document.getElementById('btn-');
+// let btnModulo = document.getElementById('btn%');
+// let btnX = document.getElementById('btnx');
+// let btn1 = document.getElementById('btn1');
+// let btn2 = document.getElementById('btn2');
+// let btn3 = document.getElementById('btn3');
+// let btn4 = document.getElementById('btn4');
+// let btn5 = document.getElementById('btn5');
+// let btn6 = document.getElementById('btn6');
+// let btn7 = document.getElementById('btn7');
+// let btn8 = document.getElementById('btn8');
+// let btn9 = document.getElementById('btn9');
+// let btnCE = document.getElementById('btnCE');
+// let btnEq = document.getElementById('btn=');
+let nb1 = document.getElementById('calcul');
+let calcul = document.getElementById('result1');
+let nb2 = document.getElementById('result2');
 let val3 = document.getElementById('result3');
 
 function getNumber(val) {
-    calcul.innerHTML += val
+    nb1.innerHTML += val
 }
-function getAdd() {
-    // calcul.innerHTML +=
+function getAddOperator(operator) {
+    calcul.innerText = operator
 }
 
-// btn1.addEventListener('click', getNumber(val1))
