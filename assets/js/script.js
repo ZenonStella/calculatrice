20701dc6792674ca5a4168ad7077bc0952a05411
Stella Zenon <stellazenon.gmail.com>
2022-05-05 12:10:17 +0200

    reset function

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,9 +9,19 @@ let line = nb1;
 
 function getNumber(val) {
     line.innerHTML += val
+// if (calcul.innerHTML != '') {
+//     nb2.innerHTML += val
+// }else{
+//     nb1.innerHTML += val
+// }
 }
 function getAddOperator(operator) {
     calcul.innerText = operator;
     line = nb2;
 }
-
+function reset() {
+    nb1.innerHTML = '';
+    nb2.innerHTML = '';
+    calcul.innerHTML = '';
+    result.innerHTML = '';
+}
